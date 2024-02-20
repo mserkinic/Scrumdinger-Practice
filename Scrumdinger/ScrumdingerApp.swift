e9517d85e89632701636e62d8a33aa1fa8c8a6bc
Mate Šerkinić <mateserkinic@Mates-MacBook-Air.local>
2024-02-20 20:02:04 +0100

    Added .gitignore file

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Mate Šerkinić on 20.02.2024..
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
